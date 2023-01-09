ab7732e53e1a355a908a7f09a67cb8e248973740
Samsational-9001 <sammchilders@gmail.com>
2023-01-08 22:58:54 -0800

    I GIT AN IMAGE TO POP UP!!! :D
    Ok, not a major accomplishment but it took FOREVER

--- samapp/lib/screens/home.dart
+++ samapp/lib/screens/home.dart
@@ -1,7 +1,6 @@
 //import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:samapp/screens/widgets/widgets.dart';
-import '../data/database.dart';
 import '../imagesDisplay/typesDisplay.dart';
 
 class Home extends StatefulWidget {
@@ -13,15 +12,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List<TypesDisplay> typesDisp = List<TypesDisplay>.empty();
+  //List<TypesDisplay> typesDisp = List<TypesDisplay>.empty();
 
   //makes the initial state to get the images for the types
-  @override
-  void initialState(){
-    typesDisp = getDispBox();
-    super.initState();
+  // @override
+  // void initialState(){
+  //   //getDispBox();
+  //   super.initState();
 
-  }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +35,7 @@ class _HomeState extends State<Home> {
           Container( 
             color: const Color(0xFFEC98C7),
             padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(children: <Widget>[
+            child: Row(children: const <Widget>[
               Expanded(
                 child: TextField(
                   decoration: InputDecoration(
@@ -51,17 +50,9 @@ class _HomeState extends State<Home> {
           SizedBox(height: 20,),
           Container(
             height: 100,
-            child: ListView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              itemCount: typesDisp.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder:  (context, index) {
-                  return TypeSection(
-                    type: typesDisp[index].type,
-                    imageUrl: typesDisp[index].imageUrl
-                  );
-              }),
+            child: Image(
+              image: AssetImage('data/display/dynamicCat'),
+            )
           )
         ],
       ),
@@ -96,3 +87,29 @@ class TypeSection extends StatelessWidget {
   }
 
 }
+
+// class getDispBox extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return Container(
+//       margin: EdgeInsets.symmetric(horizontal: 2),
+//       child: Stack(children: <Widget>[
+//         ClipRRect(
+//           borderRadius: BorderRadius.circular(20),
+//           child: Image(
+//             image: AssetImage('data/display/static.jpg')
+//           ),
+//         ),
+//         Container(
+//           color: Colors.purple,
+//           height:70, 
+//           width: 100,
+//           alignment: Alignment.center,
+//           child: Text("data/display/static.jpg", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),),
+//           ),
+//       ],
+//       ),
+//     );
+//   }
+
+// }
